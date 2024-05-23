4de212dac1516c1aa085a040cf736fa1f4868e89
healac23daw2 <060097.clot@fje.edu>
2024-05-23 14:55:03 +0200

    retoques powerpoint

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("clickArea").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+});
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
